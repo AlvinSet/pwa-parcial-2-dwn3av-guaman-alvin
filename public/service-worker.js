5768bcc7e0d376faed92885e65eeb8195848a5b1
AlvinSet <alvingm17@gmail.com>
2023-11-25 16:17:10 -0300

    feat: manifest.json
    update favicon

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,6 +8,7 @@ const urlsToCache = [
     "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
     "https://rickandmortyapi.com/api/character?page=1",
+    "/icons/favicon.png",
 
 ];
 
